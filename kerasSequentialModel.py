c30d5be6b4dd23548a1a12e58fb0829e47dc92ef
evegr4ce <evecunneen@gmail.com>
2025-09-09 10:06:48 -0600

    more updates

--- kerasSequentialModel.py
+++ kerasSequentialModel.py
@@ -2,17 +2,18 @@
 import tensorflow as tf
 import numpy as np
 import pandas as pd
-from keras.optimizers import RMSprop
 import pickle
+from keras.optimizers import RMSprop
 from keras.models import Sequential
 from keras.layers import Dense
 import matplotlib.pyplot as plt
 
 data = pd.read_csv('./DataFiles/urldata.csv')
 
-x = data.iloc[:, 1:-1]
-y = data['Label']
+x = data.iloc[:, 1:-1] # select all columns except for the first & last
+y = data['Label'] # target column
 
+# sequential model
 model = tf.keras.Sequential([
     tf.keras.Input(shape=(x.shape[1],), name="inputs"),
     tf.keras.layers.Dense(1, activation="sigmoid", name="denseLayer"),
@@ -20,10 +21,11 @@ model = tf.keras.Sequential([
 model.compile(optimizer=RMSprop(learning_rate=0.001), loss="binary_crossentropy", metrics=["accuracy"])
 model.fit(x, y, epochs=100, batch_size=32, validation_split=0.2)
 
-preds = (model.predict(x) > 0.5).astype(int)
-correct = (y == preds.flatten())
+# check performance
+loss, accuracy = model.evaluate(x, y, verbose=0)
 
-print("Accuracy:", np.mean(correct))
+print(f'Test Loss: {loss:.4f}')
+print(f'Test Accuracy: {accuracy:.4f}')
 
 filename = 'sequentialModel.pkl'
 with open(filename, 'wb') as file:
