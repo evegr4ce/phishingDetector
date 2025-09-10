f7b738329a1f4fc504f53415165f45da7343c962
marykate <m_laibhen2024@coloradocollege.edu>
2025-09-09 18:31:57 -0600

    desktop

---
+++ desktop.py
@@ -0,0 +1,49 @@
+import tkinter as tk
+import requests
+from bs4 import BeautifulSoup
+
+root = tk.Tk()
+
+#making the tkinter screen
+root.geometry("500x500")
+root.title("Phishing detector")
+
+
+def fetch_data():
+    url = url_entry.get()
+
+    if url:
+        response = requests.get(url)
+        if response.status_code == 200:# checking the satus code
+            soup = BeautifulSoup(response.text, 'html.parser')#parsing the url
+            extracted_data = soup.get_text()#extract the data
+            display_data(extracted_data)#display the data
+        else:
+            display_data("Error: unable to get data")
+    else:
+        display_data("please enter a URL")
+
+
+#replacing old content with new content
+def display_data(data):
+    result_text.delete(1.0, tk.END)
+    result_text.insert(tk.END, data)
+
+
+
+
+#setting up the tkinter page
+url_entry = tk.Entry(root, width=50)
+url_entry.pack(pady=5)
+
+#adding a button to check the url
+button = tk.Button(root, text =" Check URL", command=fetch_data)
+button.pack(pady =10)
+
+
+result_text = tk.Text(root, wrap= tk.WORD, height=10, width=60)
+result_text.pack(padx=10, pady=5)
+
+
+
+root.mainloop()
